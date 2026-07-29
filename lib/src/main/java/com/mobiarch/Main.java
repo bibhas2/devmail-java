45e205789bd819319e10e5dc25af1429eb41def2
bibhas <bibhas2@gmail.com>
2026-07-29 15:17:52 -0400

    Create folder

--- lib/src/main/java/com/mobiarch/Main.java
+++ lib/src/main/java/com/mobiarch/Main.java
@@ -1,7 +1,17 @@
 package com.mobiarch;
 
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        try {
+            Files.createDirectory(Paths.get("mail"));
+        } catch (FileAlreadyExistsException e) {
+            // Directory already exists, which is fine
+        }
+
         var loop = new IOLoop();
 
         loop.begin();
